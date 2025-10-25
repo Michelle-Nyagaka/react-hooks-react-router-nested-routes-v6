61165b6dcbb12572dfd1ab2d74fc059158067b61
Michelle Nyagaka <michellenyagaka@gmail.com>
2025-10-25 19:34:33 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
       <header>
-        
+        <NavBar />
       </header>
+      {/* 👇 This tells React Router where to render child routes */}
+      <Outlet />
     </>
   );
-};
+}
 
 export default App;
--- src/routes.js
+++ src/routes.js
@@ -1,29 +1,36 @@
+// Import components
+import App from "./App";               // 👈 Parent component
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Login from "./pages/Login";
 import UserProfile from "./pages/UserProfile";
 import ErrorPage from "./pages/ErrorPage";
 
+// Define nested routes
 const routes = [
   {
-    path: "/",
-    element: <Home />,
-    errorElement: <ErrorPage />
-  }, 
-  {
-    path: "/about",
-    element: <About />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: "/login",
-    element: <Login />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: "/profile/:id",
-    element: <UserProfile />,
-    errorElement: <ErrorPage />
+    // Parent route
+    path: "/",                      // The base path
+    element: <App />,               // App will render on every page
+    errorElement: <ErrorPage />,    // Handles any route errors
+    children: [                     // 👇 Nested routes go here
+      {
+        path: "/",                  // Default child (home page)
+        element: <Home />
+      },
+      {
+        path: "/about",
+        element: <About />
+      },
+      {
+        path: "/login",
+        element: <Login />
+      },
+      {
+        path: "/profile/:id",       // Dynamic route for user profiles
+        element: <UserProfile />
+      }
+    ]
   }
 ];
 
